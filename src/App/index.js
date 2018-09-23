8abe0c3b9203251da7d659c49a7942ec511e50f0
Damian Alexander Salgado <damianxalexander@gmail.com>
2018-09-23 15:54:23 -0400

    changed for build

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import ProjectsPage from '../ProjectsPage';
 import ContactPage from '../ContactPage';
 import Home from '../Home';
