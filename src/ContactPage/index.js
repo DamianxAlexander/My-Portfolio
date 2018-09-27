11bde1ec01018144b1c20b4ada1d19a110353aec
Damian Alexander Salgado <damianxalexander@gmail.com>
2018-09-26 21:45:35 -0400

    contact page changes

--- src/ContactPage/index.js
+++ src/ContactPage/index.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 class ContactPage extends Component {
   render() {
     return (
-      <div>
-        <h1 className="contact-header">
+        <div className="contact-content-wrapper">
+          <h1 className="contact-header">CONTACT</h1>
+          <hr className="splitter"/>
+          <h2 className="contact-info">DamianxAlexander@gmail.com</h2>
+          <h3 className="contact-info">732 - 421 - 5544</h3>
           <a href="https://www.linkedin.com/in/damiansalgado/">
             <img className="linkedin-img" alt="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" />
           </a>
-        </h1>
-      </div>
+        </div>
     )
   }
 }
--- src/Home/index.js
+++ src/Home/index.js
@@ -5,8 +5,11 @@ class Home extends Component {
     return (
       <div>
         <header>
-            <h1 className="intro-header">DAMIAN A. SALGADO<br></br>FULL STACK DEVELOPER</h1>
+            <h1 className="intro-header">DAMIAN A. SALGADO<hr className="splitter"/>FULL STACK DEVELOPER</h1>
         </header>
+        <div className="about-me-wrapper">
+          <p className="about-me">I am a proud veteran turned software developer. My time in the army has taught me to be organized and to persevere in tough situations, while my positive attitude and strong drive help provide focus and adaptability. These traits reverberate in my programming style.</p>
+        </div>
       </div>
     )
   }
