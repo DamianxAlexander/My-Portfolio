44a76e1bb1003170b809ebde4f85b5e527e11252
Damian Alexander Salgado <damianxalexander@gmail.com>
2018-11-27 22:26:39 -0500

    Updates

---
+++ precache-manifest.a7b2154b08f051117742ea3ca57afaa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e61caf1d1fec45b924c",
+    "url": "/static/js/main.7e61caf1.chunk.js"
+  },
+  {
+    "revision": "bbfcd8fbdaba60c10f8e",
+    "url": "/static/js/1.bbfcd8fb.chunk.js"
+  },
+  {
+    "revision": "7e61caf1d1fec45b924c",
+    "url": "/static/css/main.cf018452.chunk.css"
+  },
+  {
+    "revision": "fe2d3184bcf9958b82ee44e5c6e616bd",
+    "url": "/index.html"
+  }
+];
